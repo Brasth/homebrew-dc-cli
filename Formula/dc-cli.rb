19cf9571c429639007684d3a1f89d73363f09585
Huy Nguyen <huynguyen@Huys-MacBook-Pro-2.local>
2026-08-15 19:45:53 +0700

    dc-cli 0.7.2

--- Formula/dc-cli.rb
+++ Formula/dc-cli.rb
@@ -5,28 +5,28 @@
 class DcCli < Formula
   desc "Host-global helpers around @devcontainers/cli"
   homepage "https://dc.brasth.com"
-  version "0.7.1"
+  version "0.7.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.1/dc-cli-0.7.1-darwin-arm64.tar.gz"
-      sha256 "e31f5a9df636f5ca6593a80c2c7e0af9affc64c5e8b1dbe9bb9d91a29c2cac76"
+      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.2/dc-cli-0.7.2-darwin-arm64.tar.gz"
+      sha256 "006ebdd8ef7178cced275606ff8093394adba71b8986bb80a5106be7dbe3ee49"
     end
     on_intel do
-      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.1/dc-cli-0.7.1-darwin-amd64.tar.gz"
-      sha256 "bb924767fc6cc86e04bbc45a63f74e7815d4ed11a1840f1bdbcf4e44b13dcf12"
+      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.2/dc-cli-0.7.2-darwin-amd64.tar.gz"
+      sha256 "126fc7a28759805caed73baba14dade3b56b02c2c130a8f68ccf97491c3c4ca5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.1/dc-cli-0.7.1-linux-arm64.tar.gz"
-      sha256 "703a47ccc2fd389bd0c73fdf6b13a7b03155f560dd6c45ef74f0f187816692c6"
+      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.2/dc-cli-0.7.2-linux-arm64.tar.gz"
+      sha256 "c947412d2a05256231d639768f4e56fb6428cbdb40f7473b25564b87f97187ce"
     end
     on_intel do
-      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.1/dc-cli-0.7.1-linux-amd64.tar.gz"
-      sha256 "73107d612f6690d9799672b6415ab78e07d5cb3bac2813669623225b98c4fe70"
+      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.2/dc-cli-0.7.2-linux-amd64.tar.gz"
+      sha256 "b247f65c2d1e3b6a98475dcdad1e28cb26ae9f089b9250360882b0dcae6b2c31"
     end
   end
 

9ceb4b7039b00ba75f560b5d8bdd202838209603
Huy Nguyen <huynguyen@Huys-MacBook-Pro-2.local>
2026-08-15 19:24:30 +0700

    dc-cli 0.7.1
    Start failures stay visible; click hitboxes stay aligned.

--- Formula/dc-cli.rb
+++ Formula/dc-cli.rb
@@ -5,28 +5,28 @@
 class DcCli < Formula
   desc "Host-global helpers around @devcontainers/cli"
   homepage "https://dc.brasth.com"
-  version "0.7.0"
+  version "0.7.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.0/dc-cli-0.7.0-darwin-arm64.tar.gz"
-      sha256 "b6f50e5685fe98848ce7c0b5e623efdf9854487fbeade05e52e6f5fe0cc4b690"
+      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.1/dc-cli-0.7.1-darwin-arm64.tar.gz"
+      sha256 "e31f5a9df636f5ca6593a80c2c7e0af9affc64c5e8b1dbe9bb9d91a29c2cac76"
     end
     on_intel do
-      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.0/dc-cli-0.7.0-darwin-amd64.tar.gz"
-      sha256 "f243e654d347e11544d25b78f4288d695335a23b16c95d5edd999cb386269bcd"
+      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.1/dc-cli-0.7.1-darwin-amd64.tar.gz"
+      sha256 "bb924767fc6cc86e04bbc45a63f74e7815d4ed11a1840f1bdbcf4e44b13dcf12"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.0/dc-cli-0.7.0-linux-arm64.tar.gz"
-      sha256 "e069ce5a4d8287e98656228506e0a83da380dea4590d902f5dd59fceefef4ecb"
+      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.1/dc-cli-0.7.1-linux-arm64.tar.gz"
+      sha256 "703a47ccc2fd389bd0c73fdf6b13a7b03155f560dd6c45ef74f0f187816692c6"
     end
     on_intel do
-      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.0/dc-cli-0.7.0-linux-amd64.tar.gz"
-      sha256 "6fdecc725bda6c5f1d9fb3bd5faedce803d5e538b89c1dc41ad90a6d9fe8fb29"
+      url "https://github.com/Brasth/dc-cli/releases/download/v0.7.1/dc-cli-0.7.1-linux-amd64.tar.gz"
+      sha256 "73107d612f6690d9799672b6415ab78e07d5cb3bac2813669623225b98c4fe70"
     end
   end
 
